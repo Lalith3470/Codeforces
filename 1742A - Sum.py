de4e6f03b8f92add7ac0296e020af80d695bf0c4
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2023-03-30 14:41:28 +0530

    Update 1742A - Sum.py

--- 1742A - Sum.py
+++ 1742A - Sum.py
@@ -1,6 +1,5 @@
 for _ in range(int(input())):
     l=list(map(int,input().split()))
     l.sort()
-    if l[0]+l[1]==l[2]:
-        print("YES")
+    if l[0]+l[1]==l[2]:print("YES")
     else:print("NO")
